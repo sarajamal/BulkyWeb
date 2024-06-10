e4bf61f38a6c309ead4bd47d5d40ae958344cf80
سارة جمال <soso_j_kh@hotmail.com>
2024-06-10 14:13:46 +0300

    Add project files.

---
+++ Bookstore/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Bookstore.Data;
+using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+       public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryOpjects = _context.Categories.ToList();
+            return View(categoryOpjects);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _context.Categories.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Bookstore/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Bookstore.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Bookstore.Data
+{
+    public class ApplicationDbContext:DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+    
+}
