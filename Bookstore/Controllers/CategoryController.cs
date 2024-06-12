c6f5f8393c57b9ea5346251f7e910efe8bba8c8b
سارة جمال <soso_j_kh@hotmail.com>
2024-06-12 09:20:06 +0300

    #Category Pages

--- Bookstore/Controllers/CategoryController.cs
+++ Bookstore/Controllers/CategoryController.cs
@@ -16,6 +16,7 @@ namespace Bookstore.Controllers
             List<Category> categoryOpjects = _context.Categories.ToList();
             return View(categoryOpjects);
         }
+        //-----------------------Create Action ------------------------------------------
         public IActionResult Create()
         {
             return View();
@@ -23,9 +24,83 @@ namespace Bookstore.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _context.Categories.Add(obj);
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            //}
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(obj);
+                _context.SaveChanges();
+                TempData["success"] = "Category create successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //===============================================================================//
+
+        //-----------------------Edit Action --------------------------------------------
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            //her three way to get object data from database 
+            Category categoryFromDB = _context.Categories.Find(id);
+            //Category categoryFromDB2 = _context.Categories.FirstOrDefault(u=> u.Id == id);
+            //Category categoryFromDB3 = _context.Categories.Where(u=> u.Id == id).FirstOrDefault();
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(obj);
+                _context.SaveChanges();
+                TempData["success"]= "Category Edit successfully";
+                return RedirectToAction("Edit");
+            }
+            return View(obj);
+        }
+        //===============================================================================//
+        //-----------------------Delete Action -------------------------------------------
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //her three way to get object data from database 
+            Category categoryFromDB = _context.Categories.Find(id);
+            //Category categoryFromDB2 = _context.Categories.FirstOrDefault(u=> u.Id == id);
+            //Category categoryFromDB3 = _context.Categories.Where(u=> u.Id == id).FirstOrDefault();
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost ,ActionName("Delete")]
+        public IActionResult DeletePOST(int? Id)
+        {
+            Category? deleteCategory = _context.Categories.Find(Id);
+            if (deleteCategory == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(deleteCategory);
             _context.SaveChanges();
+            TempData["success"] = "Category delete successfully";
             return RedirectToAction("Index");
         }
+        //===============================================================================//
     }
 }
